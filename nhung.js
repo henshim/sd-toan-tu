5cb21889b22291276146370eaeefe86e185a705a
henshim <kamenriderzio7@gmail.com>
2020-10-07 15:12:16 +0700

    them

---
+++ nhung.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("nhập chiều rộng: ");
+inputHeight = prompt("nhập chiều cao: ");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("diện tích là: " + area);
